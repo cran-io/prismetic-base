0ece6f15a49a6611de15a2999f166daea273b6cc
Javier Velazquez Traut <javier@cran.io>
2016-04-26 19:16:13 -0300

    Adding ssh in a separate file

---
+++ ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Make sudo actually work
+HOSTNAME=$(cat /etc/hostname)
+echo "127.0.1.1 $HOSTNAME" >> /etc/hosts
+hostname $HOSTNAME
+
+if [ "$INITSYSTEM" != "on" ]; then
+  /usr/sbin/sshd -p 80 &
+fi
