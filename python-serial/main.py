05d558977370746ed10ecd98cafe8161fff5f90d
Javier Velazquez Traut <javier@cran.io>
2016-06-02 17:02:51 -0300

    Printing when a random id is read through the serial parser

--- python-serial/main.py
+++ python-serial/main.py
@@ -7,13 +7,17 @@ ser = serial.Serial('/dev/ttyACM0',57600,timeout=1s)
 while True:
         income = ser.readline()
         if '\n' in income:
-                data = income.split('\n')
+                info = income.split('\n')
                 values = data[0].split(',')
                 id=int(values[0])
                 peopleIn=int(values[1])
                 peopleOut=int(values[2])
                 data=[id,peopleIn,peopleOut]
                 print 'New data:' + str(data)
+                # ------Temporary
+                if(id!=2)
+                        print(income,file=sys.stderr)
+                # ------
                 if (peopleIn>0) or (peopleOut>0):
                         file = open("/data/"+str(int(time.time()))+" - "+values[0]+".dat",'w')
                         file.write(str(datetime.now())+'\n')
