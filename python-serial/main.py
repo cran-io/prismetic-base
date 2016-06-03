ca3853305e8842e7ce5bc1b804de97c2d424f2c4
ajurnetb <alan@cran.io>
2016-06-03 11:49:27 -0300

    Vuelta atras con el serial

--- python-serial/main.py
+++ python-serial/main.py
@@ -2,22 +2,18 @@ import serial
 import time
 from datetime import datetime
 
-ser = serial.Serial('/dev/ttyACM0',57600,timeout=1)
+ser = serial.Serial('/dev/ttyACM0',57600)
 
 while True:
         income = ser.readline()
         if '\n' in income:
-                info = income.split('\n')
+                data = income.split('\n')
                 values = data[0].split(',')
                 id=int(values[0])
                 peopleIn=int(values[1])
                 peopleOut=int(values[2])
                 data=[id,peopleIn,peopleOut]
                 print 'New data:' + str(data)
-                # ------Temporary
-                if(id!=2)
-                        print(income,file=sys.stderr)
-                # ------
                 if (peopleIn>0) or (peopleOut>0):
                         file = open("/data/"+str(int(time.time()))+" - "+values[0]+".dat",'w')
                         file.write(str(datetime.now())+'\n')
