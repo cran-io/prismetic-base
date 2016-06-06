6a8563628f9a917fb28eb592e230f6cf67092212
ajurnetb <alan@cran.io>
2016-06-06 13:15:08 -0300

    Serial arreglado para que corra

--- python-serial/main.py
+++ python-serial/main.py
@@ -2,26 +2,24 @@ import serial
 import time
 from datetime import datetime
 
+
 ser = serial.Serial('/dev/ttyACM0',57600,timeout=1)
 
 while True:
         income = ser.readline()
         if '\n' in income:
                 info = income.split('\n')
-                values = data[0].split(',')
+                values = info[0].split(',')
                 id=int(values[0])
                 peopleIn=int(values[1])
                 peopleOut=int(values[2])
                 data=[id,peopleIn,peopleOut]
-                sys.stdout.write('New data:' + str(data))
-                # ------Temporary
-                if(id!=2):
-                        sys.stderr.write(income,file=sys.stderr)
-                # ------
+                print(data)
                 if (peopleIn>0) or (peopleOut>0):
                         file = open("/data/"+str(int(time.time()))+" - "+values[0]+".dat",'w')
                         file.write(str(datetime.now())+'\n')
                         for d in data:
                                 file.write(str(d)+'\n')
                         file.close()
+
 
